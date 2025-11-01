4edbb4fb40cdb9bbe4a9513022a0b551390487c4
Hasnain <hasnainnadeem404@gmail.com>
2025-11-01 10:59:33 +0500

    fetch

---
+++ protype.js
@@ -0,0 +1,12 @@
+// prototype 
+// javascript default behaviour is prototypal behaviour so its check or find the required thing in parents grandparents and more in layers although the js find null value
+// in javascript all array string and function at the end is object and object have end of the js 
+function malntiply(num){
+    // console.log('manu')
+    return num*5
+}
+
+
+
+
+
