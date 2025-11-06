9780616e0a4b53501044e86c62d3e6bb3da01d9e
hasnain404dev <hasnainnadeem404@gmail.com>
2025-11-07 01:26:17 +0500

    new file

---
+++ callandstatic.js
@@ -0,0 +1,13 @@
+// classes in javascript 
+// its a simple syntatic sugar because under the hood new key word and this logic use
+class User{
+    // we can add properties and function in class
+    
+}
+
+
+
+
+
+
+
